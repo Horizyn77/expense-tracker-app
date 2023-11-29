32ccf8feea21d1cff3beafca96c4bef2ec61fa7a
Hidaayat <hidaayat07@gmail.com>
2023-11-29 16:30:03 +0200

    added more tests

--- test/expense-tracker.test.js
+++ test/expense-tracker.test.js
@@ -16,8 +16,8 @@ describe("Testing the Expense Tracker App", function() {
     this.timeout(20000)
     
     beforeEach(async function () {
-        //Clean all tables
-        await db.none("DELETE FROM expense")
+        //Clean all tables and reset id column
+        await db.none("TRUNCATE TABLE expense RESTART IDENTITY")
     })
     //testing adding an expense
     it("should be able to add an expense", async () => {
@@ -28,6 +28,26 @@ describe("Testing the Expense Tracker App", function() {
 
         const expense = result[0].expense
 
-        assert.deepEqual(expense, "Playstation")
+        assert.equal(expense, "Playstation")
+    })
+
+    it("should be able to remove an expense", async () => {
+        //first add an expense
+        await expenseTrackerService.addExpenseItem("Playstation", 13000, 13000, 5);
+
+        const result = await expenseTrackerService.showAllExpenses();
+
+        const expense = result[0].expense
+        //check that expense exists
+        assert.equal(expense, "Playstation")
+
+        //now remove expense
+
+        //removing expense with id 1 as it's the only item added
+        await expenseTrackerService.removeExpenseItem(1)
+
+        const result2 = await expenseTrackerService.showAllExpenses();
+        //testing if an empty array is returned after removing items
+        assert.deepEqual(result2, [])
     })
 })
