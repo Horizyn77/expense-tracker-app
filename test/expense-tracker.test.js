df25301170defba4ebd05412b9cf960fcc839cb3
Hidaayat <hidaayat07@gmail.com>
2023-11-29 16:42:52 +0200

    added another test

--- test/expense-tracker.test.js
+++ test/expense-tracker.test.js
@@ -12,16 +12,16 @@ const db = pgp(connectionString)
 
 const expenseTrackerService = ExpenseTrackerService(db);
 
-describe("Testing the Expense Tracker App", function() {
+describe("Testing the Expense Tracker App", function () {
     this.timeout(20000)
-    
+
     beforeEach(async function () {
         //Clean all tables and reset id column
         await db.none("TRUNCATE TABLE expense RESTART IDENTITY")
     })
     //testing adding an expense
     it("should be able to add an expense", async () => {
-        
+
         await expenseTrackerService.addExpenseItem("Playstation", 13000, 13000, 5);
 
         const result = await expenseTrackerService.showAllExpenses();
@@ -49,5 +49,18 @@ describe("Testing the Expense Tracker App", function() {
         const result2 = await expenseTrackerService.showAllExpenses();
         //testing if an empty array is returned after removing items
         assert.deepEqual(result2, [])
+    })
+
+    it("should be able to get all category totals", async () => {
+        //adding two expenses for category 5 once totalling 22000 altogether
+        await expenseTrackerService.addExpenseItem("Playstation", 13000, 13000, 5);
+        await expenseTrackerService.addExpenseItem("Cellphone", 9000, 9000, 5);
+        //adding another expense for category 4 weekend totalling 400
+        await expenseTrackerService.addExpenseItem("Takeout", 200, 400, 4)
+
+        const result = await expenseTrackerService.getTotalsForAllCategories();
+        const testResult = [{category_type: "weekend", total_expense: 400}, {category_type: "once-off", total_expense: 22000}]
+
+        assert.deepEqual(result, testResult)
     })
 })
